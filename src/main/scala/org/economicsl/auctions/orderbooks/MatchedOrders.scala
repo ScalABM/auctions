cdd33a03e0f152c2742f91a5cf2fd808faacb0d5
davidrpugh <david.pugh@kapsarc.org>
2017-03-02 22:05:10 +0300

    improved implementation of zipped.

--- src/main/scala/org/economicsl/auctions/orderbooks/MatchedOrders.scala
+++ src/main/scala/org/economicsl/auctions/orderbooks/MatchedOrders.scala
@@ -48,11 +48,9 @@ private[orderbooks] class MatchedOrders[A <: LimitAskOrder, B <: LimitBidOrder] 
   def zipped: Stream[(A, B)] = {
     @annotation.tailrec
     def loop(askOrders: SortedAskOrders[A], bidOrders: SortedBidOrders[B], pairedOrders: Stream[(A, B)]): Stream[(A, B)] = {
-      if (askOrders.isEmpty || bidOrders.isEmpty) {
-        pairedOrders
-      } else {
-        val pair = (askOrders.head, bidOrders.head)
-        loop(askOrders.tail, bidOrders.tail, Stream.cons(pair, pairedOrders))
+      (askOrders.headOption, bidOrders.headOption) match {
+        case (Some(askOrder), Some(bidOrder)) => loop(askOrders.tail, bidOrders.tail, Stream.cons((askOrder, bidOrder), pairedOrders))
+        case _ => pairedOrders
       }
     }
     loop(askOrders, bidOrders, Stream.empty[(A, B)])
