b98fe464072fbec15159a28fc3c68b2c29b8e118
davidrpugh <david.pugh@kapsarc.org>
2017-03-09 09:02:39 +0300

    Moved orderbooks package into the singleunit package.

--- src/main/scala/org/economicsl/auctions/multiunit/orderbooks/UnMatchedOrders.scala
+++ src/main/scala/org/economicsl/auctions/multiunit/orderbooks/UnMatchedOrders.scala
@@ -13,7 +13,7 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 */
-package org.economicsl.auctions.orderbooks
+package org.economicsl.auctions.multiunit.orderbooks
 
 import org.economicsl.auctions.{LimitAskOrder, LimitBidOrder, Tradable}
 
--- src/main/scala/org/economicsl/auctions/sandbox.sc
+++ src/main/scala/org/economicsl/auctions/sandbox.sc
@@ -1,7 +1,7 @@
 import java.util.UUID
 
 import org.economicsl.auctions._
-import org.economicsl.auctions.orderbooks.FourHeapOrderBook
+import org.economicsl.auctions.multiunit.orderbooks.FourHeapOrderBook
 
 
 /** Example `Tradable` object. */
