f4b0cf69772402d1de1fc5bf74c2b4598ed20811
davidrpugh <david.pugh@kapsarc.org>
2017-03-15 08:28:09 +0300

    Added useful compiler flags globally.

---
+++ src/main/scala/org/economicsl/auctions/Tradable.scala
@@ -0,0 +1,8 @@
+package org.economicsl.auctions
+
+/**
+  * Created by pughdr on 3/15/2017.
+  */
+trait Tradable {
+
+}
---
+++ src/main/scala/org/economicsl/auctions/singleunit/orderbooks/SortedAskOrders.scala
@@ -0,0 +1,8 @@
+package org.economicsl.auctions.singleunit.orderbooks
+
+/**
+  * Created by pughdr on 3/15/2017.
+  */
+class SortedAskOrders {
+
+}
