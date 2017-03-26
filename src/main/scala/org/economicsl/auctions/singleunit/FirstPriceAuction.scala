d46cf127fd6dd605338b53c45700ca24fa615af9
davidrpugh <david.pugh@kapsarc.org>
2017-03-26 17:38:49 +0300

    Updated existing classes to inherit from new traits.

--- src/main/scala/org/economicsl/auctions/singleunit/FirstPriceAuction.scala
+++ src/main/scala/org/economicsl/auctions/singleunit/FirstPriceAuction.scala
@@ -1,22 +1,39 @@
 package org.economicsl.auctions.singleunit
 
-import org.economicsl.auctions.{Price, Tradable}
+import org.economicsl.auctions.Tradable
 import org.economicsl.auctions.singleunit.orderbooks.FourHeapOrderBook
-import org.economicsl.auctions.singleunit.pricing.PricingRule
+import org.economicsl.auctions.singleunit.pricing.AskQuotePricingRule
 
 
-class FirstPriceAuction[T <: Tradable] private(orderBook: FourHeapOrderBook[T]) extends DoubleAuction[T] {
+class FirstPriceAuction[T <: Tradable] private(orderBook: FourHeapOrderBook[T]) extends AuctionLike[T, FirstPriceAuction[T]] {
 
-  def insert(order: LimitAskOrder[T]): DoubleAuction[T] = ???
+  def insert(order: LimitBidOrder[T]): FirstPriceAuction[T] = {
+    new FirstPriceAuction(orderBook + order)
+  }
 
-  def insert(order: LimitBidOrder[T]): DoubleAuction[T] = ???
+  def remove(order: LimitBidOrder[T]): FirstPriceAuction[T] = {
+    new FirstPriceAuction(orderBook - order)
+  }
 
-  def remove(order: LimitAskOrder[T]): DoubleAuction[T] = ???
+  def clear: (Option[Stream[Fill[T]]], FirstPriceAuction[T]) = {
+    p(orderBook) match {
+      case Some(price) =>
+        val (pairedOrders, newOrderBook) = orderBook.takeAllMatched
+        val fills = pairedOrders.map { case (askOrder, bidOrder) => Fill(askOrder, bidOrder, price) }
+        (Some(fills), new FirstPriceAuction(newOrderBook))
+      case None => (None, new FirstPriceAuction(orderBook))
+    }
+  }
 
-  def remove(order: LimitBidOrder[T]): DoubleAuction[T] = ???
+  private[this] val p: AskQuotePricingRule[T] = new AskQuotePricingRule[T]
 
-  def clear(p: PricingRule[T, Price]): (Option[Stream[Fill[T]]], DoubleAuction[T]) = {
-    ???
+}
+
+
+object FirstPriceAuction{
+
+  def withEmptyOrderBook[T <: Tradable](implicit askOrdering: Ordering[LimitAskOrder[T]], bidOrdering: Ordering[LimitBidOrder[T]]): FirstPriceAuction[T] = {
+    new FirstPriceAuction[T](FourHeapOrderBook.empty[T](askOrdering, bidOrdering))
   }
 
 }
