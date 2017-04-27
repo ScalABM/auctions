9f95626842a621839cc7a2193abfc80c5b571d8e
davidrpugh <david.pugh@kapsarc.org>
2017-04-27 13:30:25 +0300

    Try a bash script.

---
+++ publish.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+if [ "$TRAVIS_BRANCH" == "develop" ]; then
+
+  sbt 'set credentials += Credentials("Sonatype Nexus Repository Manager", "oss.sonatype.org", System.getenv("$(SONATYPE_USERNAME)"), System.getenv("$(SONATYPE_PASSWORD)"))'
+  sbt publish
+
+fi
