31c72e765372d1df3bee4ff9dd0b750a0e1021e5
davidrpugh <david.pugh@kapsarc.org>
2017-04-27 23:19:08 +0300

    Fixed silly typo.

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -3,6 +3,6 @@
 #if [ $TRAVIS_BRANCH == "develop" && $TRAVIS_EVENT_TYPE == "cron" ]; then
 if [ $TRAVIS_BRANCH == "develop" ]; then
 
-  sbt 'set credentials += Credentials("Sonatype Nexus Repository Manager", "oss.sonatype.org", System.getenv("SONATYPE_USERNAME"), System.getenv("SONATYPE_USERNAME"))' publish
+  sbt 'set credentials += Credentials("Sonatype Nexus Repository Manager", "oss.sonatype.org", System.getenv("SONATYPE_USERNAME"), System.getenv("SONATYPE_PASSWORD"))' publish
 
 fi
