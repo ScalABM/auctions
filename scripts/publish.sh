7ba074b9b55513140c7f312548334f66d88979b8
davidrpugh <david.pugh@kapsarc.org>
2017-04-27 14:11:53 +0300

    added cron job condition.

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
-if [ "$TRAVIS_BRANCH" == "develop" ]; then
+#if [ $TRAVIS_BRANCH == "develop" && $TRAVIS_EVENT_TYPE == "cron" ]; then
+if [ $TRAVIS_BRANCH == "develop" ]; then
 
   sbt 'set credentials += Credentials("Sonatype Nexus Repository Manager", "oss.sonatype.org", System.getenv("$(SONATYPE_USERNAME)"), System.getenv("$(SONATYPE_PASSWORD)"))'
   sbt publish
