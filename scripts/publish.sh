fb4dd5c9f9d7d58c245141f8aec5b8d200e38c31
davidrpugh <david.pugh@kapsarc.org>
2017-04-27 14:33:50 +0300

    Progress?

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/env bash
 
 #if [ $TRAVIS_BRANCH == "develop" && $TRAVIS_EVENT_TYPE == "cron" ]; then
-if [ $TRAVIS_BRANCH == "develop" ]; then
+if [ "$(TRAVIS_BRANCH)" == "develop" ]; then
 
-  sbt 'set credentials += Credentials("Sonatype Nexus Repository Manager", "oss.sonatype.org", System.getenv("$(SONATYPE_USERNAME)"), System.getenv("$(SONATYPE_PASSWORD)"))'
-  sbt publish
+  REALM="Sonatype Nexus Repository Manager"
+  HOST="oss.sonatype.org"
+  sbt 'set credentials += Credentials("$(REALM)", "$(HOST)", "$(SONATYPE_USERNAME)", "$(SONATYPE_PASSWORD)")' ++$(TRAVIS_SCALA_VERSION) publish
 
 fi
