21e5c8caa56dcb0e78e05fd16cff3e5b516c0c50
davidrpugh <david.pugh@kapsarc.org>
2017-04-27 14:48:34 +0300

    Try again with variables.

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 
 #if [ $TRAVIS_BRANCH == "develop" && $TRAVIS_EVENT_TYPE == "cron" ]; then
-if [ "$(TRAVIS_BRANCH)" == "develop" ]; then
+if [ $TRAVIS_BRANCH == "develop" ]; then
 
   REALM="Sonatype Nexus Repository Manager"
   HOST="oss.sonatype.org"
-  sbt 'set credentials += Credentials("$(REALM)", "$(HOST)", "$(SONATYPE_USERNAME)", "$(SONATYPE_PASSWORD)")' ++$(TRAVIS_SCALA_VERSION) publish
+  sbt 'set credentials += Credentials($REALM, $HOST, $SONATYPE_USERNAME, $SONATYPE_PASSWORD)' publish
 
 fi
