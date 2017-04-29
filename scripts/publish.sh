b240c1dc38f9136c87ee2171e4bc307689dc5a3b
herculesl <economicsl.ci@gmail.com>
2017-04-29 09:40:21 +0300

    Revert to original path

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/bash
 set -e  # Needed in order for Travis CI to report failure if publish.sh fails!
 
 if [ $TRAVIS_BRANCH == "develop" ] && [ $TRAVIS_EVENT_TYPE == "cron" ]; then
