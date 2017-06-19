d058bd5803c3919f64e998b1c10fc3859d0ff1c0
davidrpugh <david.pugh@kapsarc.org>
2017-06-19 09:50:17 +0300

    Added examples of periodic double auctions.

---
+++ src/main/scala/org/economicsl/auctions/actors/SealedBidDoubleAuctionActor.scala
@@ -0,0 +1,64 @@
+package org.economicsl.auctions.actors
+
+import akka.actor.{Actor, ActorLogging, ActorRef}
+import org.economicsl.auctions.singleunit.orders.{AskOrder, BidOrder}
+import org.economicsl.auctions.singleunit.pricing.PricingPolicy
+import org.economicsl.auctions.singleunit.twosided.SealedBidDoubleAuction
+import org.economicsl.core.Tradable
+
+import scala.util.{Failure, Success}
+
+
+final class SealedBidDoubleAuctionActor[T <: Tradable](pricingPolicy: PricingPolicy[T],
+                                                       tickSize: Long,
+                                                       settlementService: ActorRef)
+    extends Actor
+    with ActorLogging
+    with Timestamper {
+
+  def timestamp(): Long = {
+    currentTimeMillis()
+  }
+
+  def handleAskOrder: Receive = {
+    case order: AskOrder[T] =>
+      auction.insert(order) match {
+        case Success(updated) =>
+          sender() ! Accepted(timestamp(), order)
+          auction = updated
+        case Failure(ex) =>
+          sender() ! Rejected(timestamp(), order, ex)
+      }
+  }
+
+  def handleBidOrder: Receive = {
+    case order: BidOrder[T] =>
+      auction.insert(order) match {
+        case Success(updated) =>
+          sender() ! Accepted(timestamp(), order)
+          auction = updated
+        case Failure(ex) =>
+          sender() ! Rejected(timestamp(), order, ex)
+      }
+  }
+
+  def handleOrder: Receive = {
+    handleAskOrder orElse handleBidOrder
+  }
+
+  def handleClearRequest: Receive = {
+    case ClearRequest =>
+      val results = auction.clear
+      results.fills.foreach(fills => settlementService ! fills)
+      auction = results.residual
+  }
+
+  def receive: Receive = {
+    handleOrder orElse handleClearRequest
+  }
+
+  private[this] var auction: SealedBidDoubleAuction.DiscriminatoryPricingImpl[T] = {
+    SealedBidDoubleAuction.withDiscriminatoryPricing(pricingPolicy, tickSize)
+  }
+
+}
