87f10204a359680d8897379fb88cea159654dedc
davidrpugh <david.pugh@kapsarc.org>
2017-07-25 09:07:35 +0300

    Success...now for testing!

--- src/main/scala/org/economicsl/auctions/singleunit/Auction.scala
+++ src/main/scala/org/economicsl/auctions/singleunit/Auction.scala
@@ -72,7 +72,13 @@ trait Auction[T <: Tradable, A <: Auction[T, A]]
     * @return
     * @note this method is necessary in order to parallelize auction simulations.
     */
-  def combineWith(that: A): A
+  def combineWith(that: A): A = {
+    require(tradable.equals(that.tradable), "Auctions can only be combined if they are for the same Tradable!")
+    val combinedOrderBooks = orderBook.combineWith(that.orderBook)
+    val withCombinedOrderBooks = withOrderBook(combinedOrderBooks)
+    val updatedTickSize = tickSize * that.tickSize  // todo compute least-common-multiple of tick sizes! overflow!
+    withCombinedOrderBooks.withTickSize(updatedTickSize)
+  }
 
   /** Create a new instance of type class `A` whose order book contains an additional `BidOrder`.
     *
